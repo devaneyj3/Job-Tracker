43b5d5ee0dc25646f10463ed513386b219fe47cd
Jordan Devaney <jordandevaney@Jordans-MacBook-Pro.local>
2020-03-13 17:29:01 -0400

    add pokemon list component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PokemonList from './components/PokemonList';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Gotta Catch 'em All</h1>
+      <PokemonList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PokemonList.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PokemonList = () => {
+    return (
+        <div className='pokemon_list'>
+            <h1>PokemonList</h1>
+        </div>
+    )
+}
+
+export default PokemonList;
