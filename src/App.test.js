5dde385811a986339343ae45e58b89f51df100f7
Jordan Devaney <jordandevaney@Jordans-MacBook-Pro.local>
2020-03-22 17:38:04 -0400

    renders job form

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import '@testing-library/jest-dom'
+import '@testing-library/jest-dom';
 import App from './App';
+import JobForm from './components/JobForm';
 
 test('Render App Component', () => {
   render(<App />);
 });
+
+test('render Job Form', () => {
+  render(<JobForm/>);
+})
